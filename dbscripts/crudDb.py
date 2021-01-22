64bb7507b22a5ccb0afe5874b3b99ebf3d36a326
BigS666 <cpsiat.spoirier@gmail.com>
2021-01-22 22:54:51 +0100

    api finalized
    add list all, delete endpoints
    new plugins flake8

--- dbscripts/crudDb.py
+++ dbscripts/crudDb.py
@@ -1,56 +1,107 @@
 import sqlite3
 
+
 def getNextId():
 
     # connect db
-    conn = sqlite3.connect('thumbnails.db')
+    conn = sqlite3.connect("thumbnails.db")
     curseur = conn.cursor()
     nombre = 1
 
     try:
-        createTableString = 'SELECT MAX(id) FROM thumbnails;'
+        createTableString = "SELECT MAX(id) FROM thumbnails;"
         resulSet = curseur.execute(createTableString)
     except sqlite3.OperationalError as exeption:
-        
-        return(str(exeption) + "Bdd non crée")
+
+        return str(exeption) + "Bdd non crée"
 
     try:
         for resultat in resulSet:
             print(resultat)
             nombre = int(resultat[0])
-            nombre +=1
-    except TypeError as exception:    
+            nombre += 1
+    except TypeError as exception:
         print(str(exception) + " : pas de resultat dans la base")
 
     conn.close()
     return nombre
 
-def saveImage(id,state,meta):
-    conn = sqlite3.connect('thumbnails.db')
+
+def saveImage(id, state, meta):
+    conn = sqlite3.connect("thumbnails.db")
     curseur = conn.cursor()
-    link = "http://127.0.0.1:5000/thumbnails/"+str(id)+".jpg"
-    insertString = '''insert into thumbnails (id,state,metadata,link) values (?,?,?,?)'''
-    params = (id,str(state),str(meta),str(link))
-    curseur.execute(insertString,params)
+    link = "http://127.0.0.1:5000/thumbnails/" + str(id) + ".jpg"
+    insertString = (
+        """insert into thumbnails (id,state,metadata,link) values (?,?,?,?)"""
+    )
+    params = (id, str(state), str(meta), str(link))
+    curseur.execute(insertString, params)
     conn.commit()
     conn.close()
 
+
 def getImage(id):
-    conn = sqlite3.connect('thumbnails.db')
+    conn = sqlite3.connect("thumbnails.db")
     curseur = conn.cursor()
-    selectString = '''select state,metadata,link from thumbnails where id=?'''
+    selectString = """select state,metadata,link from thumbnails where id=?"""
     params = (id,)
-    results = curseur.execute(selectString,params)
-    retour = "Aucune image : " +str(id)
+    results = curseur.execute(selectString, params)
+    retour = "Aucune image : " + str(id)
     for resultat in results:
         try:
             state = str(resultat[0])
             metadata = str(resultat[1])
             link = str(resultat[2])
-            retour = '{"state":'+state+', "metadata":'+metadata+', "link":'+link+'}'
+            retour = (
+                '{"state":'
+                + state
+                + ', "metadata":'
+                + metadata
+                + ', "link":'
+                + link
+                + "}"
+            )
         except TypeError as identifier:
             print(str(identifier))
-            retour = "Erreur lors de la récupération des informations de l'image : "+str(id) 
-        
+            retour = (
+                "Erreur lors de la récupération des informations de l'image : "
+                + str(id)
+            )
+
     conn.close()
     return retour
+
+def getAllImages():
+    conn = sqlite3.connect("thumbnails.db")
+    curseur = conn.cursor()
+    selectString = """select state,metadata,link from thumbnails"""
+    results = curseur.execute(selectString)
+    retour = "{"
+    for resultat in results:
+        try:
+            state = str(resultat[0])
+            metadata = str(resultat[1])
+            link = str(resultat[2])
+            retour += '{"state": "' + state + '", "metadata":"' + metadata  + '", "link":"' + link  + '"},'
+        except TypeError as identifier:
+            print(str(identifier))
+            retour = (
+                "Erreur lors de la récupération des informations des images "
+                + str(id)
+            )
+    #retrait de la derniere virgule
+    retour = retour[:-1]
+    retour += "}"
+
+    conn.close()
+    return retour
+
+def deleteImage(id):
+    conn = sqlite3.connect("thumbnails.db")
+    curseur = conn.cursor()
+    selectString = """delete from thumbnails where id=?"""
+    params = (id,)
+    curseur.execute(selectString, params)
+
+    conn.commit()
+    conn.close()
--- imageThumbnailingApp.py
+++ imageThumbnailingApp.py
@@ -1,89 +1,110 @@
-import os
 import io
-from flask import Flask
 import json
-from flask import jsonify
-from flask import request
-from flask import Response
-from flask import send_file
-from flask import send_from_directory
-import imageWorking
+import os
+
+from flask import (Flask, Response, jsonify, request, send_file,
+                   send_from_directory)
+
 import dbscripts.crudDb
+import imageWorking
 
 app = Flask(__name__)
 
-@app.route('/images', methods=['POST'])
+
+@app.route("/images", methods=["POST"])
 def soumissionImage():
-    """ upload a new image, responds an image ID
+    """upload a new image, responds an image ID
 
     >>> curl -F mydata=@Capture.PNG http://127.0.0.1:5000/images \n
     >{"id":42}
     """
-    retour =''
+    retour = ""
 
-    #testing existance of 'mydata' as attached file to upload
-    if 'mydata' not in request.files:
+    # testing existance of 'mydata' as attached file to upload
+    if "mydata" not in request.files:
         return "No selected 'mydata' File to upload"
-    
-    file = request.files['mydata']
 
-    #testing filname not empty
-    if file.filename == '':
-        return 'No file selected (name empty)'
+    file = request.files["mydata"]
 
-    #Obtention du prochain id disponnible
+    # testing filname not empty
+    if file.filename == "":
+        return "No file selected (name empty)"
+
+    # Obtention du prochain id disponnible
     id = dbscripts.crudDb.getNextId()
 
-    if(type(id) == int):
+    if type(id) == int:
 
-        dbscripts.crudDb.saveImage(id,'pending',imageWorking.getAllInfo(file))
+        dbscripts.crudDb.saveImage(id, "pending", imageWorking.getAllInfo(file))
 
-        #miniaturisation
+        # miniaturisation
         thubnailedImg = imageWorking.thumbnailing(file)
 
-        #enregistrement de l'image
-        thubnailedImg.save("static/"+ str(id) + ".jpg","JPEG")
+        # enregistrement de l'image
+        thubnailedImg.save("static/" + str(id) + ".jpg", "JPEG")
 
-        retour = '{"id":'+str(id)+'}'
-    else :
-        return ("erreur interne")
+        retour = '{"id":' + str(id) + "}"
+    else:
+        return "erreur interne"
 
     return retour
 
-@app.route('/images', methods=['GET'])
+
+@app.route("/images", methods=["GET"])
 def listImages():
-    """ Lists all the stockedimages
+    """Lists all the stockedimages
 
     >>> curl http://127.0.0.1:5000/images
     {{id:42,metadatas:{},thumbadress:'http://127.0.0.1:5000/thumbnails/42.jpg'}}
     """
-    #TODO
-    params=request.args
-
-    retour = params
-    print(retour)
+    retour = dbscripts.crudDb.getAllImages()
 
     return retour
 
-@app.route('/images/<int:image_id>', methods=['GET'])
+
+@app.route("/images/<int:image_id>", methods=["GET"])
 def infoImage(image_id):
-    """ describe image processing state (pending, success, failure) metadata and links to thumbnail
+    """describe image processing state (pending, success, failure) metadata and links to thumbnail
 
     >>> curl http://127.0.0.1:5000/images/42
-    {{"state":success,"metadatas":{},"link":'http://127.0.0.1:5000/thumbnails/42.jpg'}}
+    {"state":success,"metadatas":{},"link":'http://127.0.0.1:5000/thumbnails/42.jpg'}
     """
+    #recherche en base les donnees de l'image souhaitee
     retour = dbscripts.crudDb.getImage(image_id)
 
     return retour
 
-@app.route('/thumbnails/<int:thumbnail_id>.jpg', methods=['GET'])
+
+@app.route("/thumbnails/<int:thumbnail_id>.jpg", methods=["GET"])
 def getThumbnail(thumbnail_id):
-    """ read the requested generated thumbnail
+    """read the requested generated thumbnail
 
     >>> curl http://127.0.0.1:5000/thumbnails/42.jpg
     <img style="-webkit-user-select: none;margin: auto;" src="http://127.0.0.1:5000/thumbnails/42.jpg">
     """
+    #construction du nom du fichier correspondant a l'id souhaite
+    name = str(thumbnail_id) + ".jpg"
 
-    name =  str(thumbnail_id) + ".jpg"
-
+    #envoi de l'image stockee dans le dossier static
     return send_from_directory("static/", name)
+
+
+@app.route("/images/<int:image_id>", methods=["DELETE"])
+def deleteImage(image_id):
+    """delete the selected image
+
+    >>> curl -X DELETE http://127.0.0.1:5000/images/1
+    {{"state":"success"}}
+    """
+    retour = '{ {"state":"success"} }'
+
+    #suppression de la ligne dans la base
+    dbscripts.crudDb.deleteImage(image_id)
+
+    #suppression du fichier image
+    if os.path.exists("static/" + str(image_id) + ".jpg"):
+        os.remove("static/" + str(image_id) + ".jpg")
+    else:
+        retour = '{ {"state":"echec de suppression du fichier"} }'
+
+    return retour
